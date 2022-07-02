deeaca1f79bd940e1e41308948098d13d8296e03
Zolisa Bleki <44142765+zoj613@users.noreply.github.com>
2022-07-03 00:26:46 +0200

    Remove `multivariate_normal_bhattacharya2016`.
    The algorithm in Bhattacharya (2016) can be seen as a special case of the
    `multivariate_normal_cong2017` function where `omega` is the identity
    matrix. This commit removes the redundant implementation of the
    Bhattacharya (2016) algorithm and then adds a unittest for this special
    case.

--- tests/test_dists.py
+++ tests/test_dists.py
@@ -6,7 +6,6 @@ from aesara.sparse.basic import as_sparse
 from scipy.sparse import csc_matrix
 
 from aemcmc.dists import (
-    multivariate_normal_bhattacharya2016,
     multivariate_normal_cong2017,
     multivariate_normal_rue2005,
     polyagamma,
@@ -52,32 +51,6 @@ def test_multivariate_normal_rue2005():
     np.testing.assert_allclose(np.var(samples, axis=0), var, atol=0.1)
 
 
-def test_multivariate_normal_bhattacharya2016():
-    nrng = np.random.default_rng(54321)
-    X = nrng.standard_normal(size=(50, 100))
-    phi = np.linalg.cholesky(X @ X.T).T
-    D = nrng.random(phi.shape[1])
-    alpha = nrng.random(phi.shape[0])
-    precision = phi.T @ phi
-    precision[np.diag_indices_from(precision)] += 1.0 / D
-    cov = np.linalg.inv(precision)
-    mean = cov @ phi.T @ alpha
-
-    srng = at.random.RandomStream(12345)
-
-    def update():
-        return multivariate_normal_bhattacharya2016(
-            srng, at.as_tensor(D), at.as_tensor(phi), at.as_tensor(alpha)
-        )
-
-    samples_out, updates = aesara.scan(update, n_steps=10000)
-    sampling_fn = aesara.function((), samples_out, updates=updates)
-    samples = sampling_fn()
-
-    np.testing.assert_allclose(np.mean(samples, axis=0), mean, atol=0.05)
-    np.testing.assert_allclose(np.var(samples, axis=0, ddof=1), np.diag(cov), atol=0.05)
-
-
 def test_multivariate_normal_cong2017():
     nrng = np.random.default_rng(54321)
     X = nrng.standard_normal(size=(50, 100))
@@ -98,6 +71,41 @@ def test_multivariate_normal_cong2017():
             at.as_tensor(omega),
             at.as_tensor(phi.T),
             at.as_tensor(t),
+        )
+
+    samples_out, updates = aesara.scan(update, n_steps=10000)
+    sampling_fn = aesara.function((), samples_out, updates=updates)
+    samples = sampling_fn()
+
+    np.testing.assert_allclose(np.mean(samples, axis=0), mean, atol=0.05)
+    np.testing.assert_allclose(np.var(samples, axis=0, ddof=1), np.diag(cov), atol=0.05)
+
+
+def test_multivariate_normal_bhattacharya2016_via_cong2017():
+    """This test case is to test if the algorithm from Cong et al (2017) can
+    be used realiably to implement one described in Bhattacharya (2016) as a
+    special case when ``omega`` is the identity matrix.
+    """
+    nrng = np.random.default_rng(54321)
+    X = nrng.standard_normal(size=(50, 100))
+    phi = np.linalg.cholesky(X @ X.T).T
+    D = nrng.random(phi.shape[1])
+    alpha = nrng.random(phi.shape[0])
+    precision = phi.T @ phi
+    precision[np.diag_indices_from(precision)] += 1.0 / D
+    cov = np.linalg.inv(precision)
+    mean = cov @ phi.T @ alpha
+
+    srng = at.random.RandomStream(12345)
+
+    def update():
+        def multivariate_normal_bhattacharya2016(rng, D, phi, alpha):
+            return multivariate_normal_cong2017(
+                rng, 1 / D, at.ones(phi.shape[0]), phi, alpha
+            )
+
+        return multivariate_normal_bhattacharya2016(
+            srng, at.as_tensor(D), at.as_tensor(phi), at.as_tensor(alpha)
         )
 
     samples_out, updates = aesara.scan(update, n_steps=10000)
