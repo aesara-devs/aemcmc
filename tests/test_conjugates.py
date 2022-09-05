e579992b2b16f36da76dbff24d741ad1d30be279
Rémi Louf <remi@thetypicalset.com>
2022-09-05 22:24:37 +0200

    Add closed-form posterior for gamma-poisson observation model

--- tests/test_conjugates.py
+++ tests/test_conjugates.py
@@ -6,7 +6,50 @@ from aesara.tensor.random import RandomStream
 from kanren import run
 from unification import var
 
-from aemcmc.conjugates import beta_binomial_conjugateo
+from aemcmc.conjugates import beta_binomial_conjugateo, gamma_poisson_conjugateo
+
+
+def test_gamma_poisson_conjugate_contract():
+    """Produce the closed-form posterior for the poisson observation model with
+    a gamma prior.
+
+    """
+    srng = RandomStream(0)
+
+    alpha_tt = at.scalar("alpha")
+    beta_tt = at.scalar("beta")
+    z_rv = srng.gamma(alpha_tt, beta_tt)
+
+    Y_rv = srng.poisson(z_rv)
+    y_vv = Y_rv.clone()
+    y_vv.name = "y"
+
+    q_lv = var()
+    (posterior_expr,) = run(1, q_lv, gamma_poisson_conjugateo(y_vv, Y_rv, q_lv))
+    posterior = eval_if_etuple(posterior_expr)
+    aesara.dprint(posterior)
+
+    assert isinstance(posterior.owner.op, type(at.random.gamma))
+
+
+@pytest.mark.xfail(
+    reason="Op.__call__ does not dispatch to Op.make_node for some RandomVariable and etuple evaluation returns an error"
+)
+def test_gamma_poisson_conjugate_expand():
+    """Expand a contracted beta-binomial observation model."""
+
+    srng = RandomStream(0)
+
+    alpha_tt = at.scalar("alpha")
+    beta_tt = at.scalar("beta")
+    y_vv = at.iscalar("y")
+    Y_rv = srng.gamma(alpha_tt + y_vv, beta_tt + 1)
+
+    e_lv = var()
+    (expanded_expr,) = run(1, e_lv, gamma_poisson_conjugateo(e_lv, y_vv, Y_rv))
+    expanded = eval_if_etuple(expanded_expr)
+
+    assert isinstance(expanded.owner.op, type(at.random.gamma))
 
 
 def test_beta_binomial_conjugate_contract():
