10fe707e5bf785d9b05f7f82577d2f511a8ce789
Rémi Louf <remilouf@gmail.com>
2022-10-13 15:40:17 +0200

    Make `construct_sampler` available in the `aemcmc` namespace

--- aemcmc/__init__.py
+++ aemcmc/__init__.py
@@ -2,6 +2,12 @@ from . import _version
 
 __version__ = _version.get_versions()["version"]
 
+
+from aemcmc.basic import construct_sampler
+
+# isort: off
 # Register rewrite databases
 import aemcmc.conjugates
 import aemcmc.gibbs
+
+# isort: on
