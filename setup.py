77aea6747f51686233caf96ad6f955f118228a6f
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2023-02-03 16:57:53 -0600

    Remove Python 3.7 support

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     long_description=open("README.rst").read() if exists("README.rst") else "",
     long_description_content_type="text/x-rst",
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
@@ -60,7 +60,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
