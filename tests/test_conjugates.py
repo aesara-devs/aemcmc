82beb059f769dce1068cd72f78e09f79da1e7dd5
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2023-05-15 17:59:28 -0500

    Remove extra argument from conjugate relations

--- tests/test_conjugates.py
+++ tests/test_conjugates.py
@@ -1,8 +1,8 @@
 import aesara
 import aesara.tensor as at
 import pytest
-from aesara.graph.rewriting.unify import eval_if_etuple
 from aesara.tensor.random import RandomStream
+from etuples import etuplize
 from kanren import run
 from unification import var
 
@@ -14,23 +14,17 @@ from aemcmc.conjugates import (
 
 
 def test_gamma_poisson_conjugate_contract():
-    """Produce the closed-form posterior for the poisson observation model with
-    a gamma prior.
-
-    """
+    """Produce the closed-form posterior for the poisson observation model with a gamma prior."""
     srng = RandomStream(0)
 
     alpha_tt = at.scalar("alpha")
     beta_tt = at.scalar("beta")
     z_rv = srng.gamma(alpha_tt, beta_tt)
-
     Y_rv = srng.poisson(z_rv)
-    y_vv = Y_rv.clone()
-    y_vv.name = "y"
 
     q_lv = var()
-    (posterior_expr,) = run(1, q_lv, gamma_poisson_conjugateo(y_vv, Y_rv, q_lv))
-    posterior = eval_if_etuple(posterior_expr)
+    (posterior_expr,) = run(1, q_lv, gamma_poisson_conjugateo(Y_rv, q_lv))
+    posterior = posterior_expr.evaled_obj
 
     assert isinstance(posterior.owner.op, type(at.random.gamma))
 
@@ -47,19 +41,17 @@ def test_gamma_poisson_conjugate_expand():
     beta_tt = at.scalar("beta")
     y_vv = at.iscalar("y")
     Y_rv = srng.gamma(alpha_tt + y_vv, beta_tt + 1)
+    Y_et = etuplize(Y_rv)
 
     e_lv = var()
-    (expanded_expr,) = run(1, e_lv, gamma_poisson_conjugateo(e_lv, y_vv, Y_rv))
-    expanded = eval_if_etuple(expanded_expr)
+    (expanded_expr,) = run(1, e_lv, gamma_poisson_conjugateo(e_lv, Y_et))
+    expanded = expanded_expr.evaled_obj
 
     assert isinstance(expanded.owner.op, type(at.random.gamma))
 
 
 def test_beta_binomial_conjugate_contract():
-    """Produce the closed-form posterior for the binomial observation model with
-    a beta prior.
-
-    """
+    """Produce the closed-form posterior for the binomial observation model with a beta prior."""
     srng = RandomStream(0)
 
     alpha_tt = at.scalar("alpha")
@@ -68,17 +60,15 @@ def test_beta_binomial_conjugate_contract():
 
     n_tt = at.iscalar("n")
     Y_rv = srng.binomial(n_tt, p_rv)
-    y_vv = Y_rv.clone()
-    y_vv.tag.name = "y"
 
     q_lv = var()
-    (posterior_expr,) = run(1, q_lv, beta_binomial_conjugateo(y_vv, Y_rv, q_lv))
-    posterior = eval_if_etuple(posterior_expr)
+    (posterior_expr,) = run(1, q_lv, beta_binomial_conjugateo(Y_rv, q_lv))
+    posterior = posterior_expr.evaled_obj
 
     assert isinstance(posterior.owner.op, type(at.random.beta))
 
     # Build the sampling function and check the results on limiting cases.
-    sample_fn = aesara.function((alpha_tt, beta_tt, y_vv, n_tt), posterior)
+    sample_fn = aesara.function((alpha_tt, beta_tt, Y_rv, n_tt), posterior)
     assert sample_fn(1.0, 1.0, 1000, 1000) == pytest.approx(
         1.0, abs=0.01
     )  # only successes
@@ -100,17 +90,14 @@ def test_beta_binomial_conjugate_expand():
     Y_rv = srng.beta(alpha_tt + y_vv, beta_tt + n_tt - y_vv)
 
     e_lv = var()
-    (expanded_expr,) = run(1, e_lv, beta_binomial_conjugateo(e_lv, y_vv, Y_rv))
-    expanded = eval_if_etuple(expanded_expr)
+    (expanded_expr,) = run(1, e_lv, beta_binomial_conjugateo(e_lv, Y_rv))
+    expanded = expanded_expr.evaled_obj
 
     assert isinstance(expanded.owner.op, type(at.random.beta))
 
 
 def test_beta_negative_binomial_conjugate_contract():
-    """Produce the closed-form posterior for the binomial observation model with
-    a beta prior.
-
-    """
+    """Produce the closed-form posterior for the binomial observation model with a beta prior."""
     srng = RandomStream(0)
 
     alpha_tt = at.scalar("alpha")
@@ -119,19 +106,15 @@ def test_beta_negative_binomial_conjugate_contract():
 
     n_tt = at.iscalar("n")
     Y_rv = srng.negative_binomial(n_tt, p_rv)
-    y_vv = Y_rv.clone()
-    y_vv.tag.name = "y"
 
     q_lv = var()
-    (posterior_expr,) = run(
-        1, q_lv, beta_negative_binomial_conjugateo(y_vv, Y_rv, q_lv)
-    )
-    posterior = eval_if_etuple(posterior_expr)
+    (posterior_expr,) = run(1, q_lv, beta_negative_binomial_conjugateo(Y_rv, q_lv))
+    posterior = posterior_expr.evaled_obj
 
     assert isinstance(posterior.owner.op, type(at.random.beta))
 
     # Build the sampling function and check the results on limiting cases.
-    sample_fn = aesara.function((alpha_tt, beta_tt, y_vv, n_tt), posterior)
+    sample_fn = aesara.function((alpha_tt, beta_tt, Y_rv, n_tt), posterior)
     assert sample_fn(1.0, 1.0, 1000, 0) == pytest.approx(
         1.0, abs=0.01
     )  # only successes
@@ -153,7 +136,7 @@ def test_beta_negative_binomial_conjugate_expand():
     Y_rv = srng.beta(alpha_tt + y_vv, beta_tt + n_tt)
 
     e_lv = var()
-    (expanded_expr,) = run(1, e_lv, beta_negative_binomial_conjugateo(e_lv, y_vv, Y_rv))
-    expanded = eval_if_etuple(expanded_expr)
+    (expanded_expr,) = run(1, e_lv, beta_negative_binomial_conjugateo(e_lv, Y_rv))
+    expanded = expanded_expr
 
     assert isinstance(expanded.owner.op, type(at.random.beta))
